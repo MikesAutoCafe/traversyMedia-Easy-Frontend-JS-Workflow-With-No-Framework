25fe2e85c2466ae075345511d3563ca535624313
unknown <mikeg.4224@gmail.com>
2020-05-26 18:03:25 -0400

    User template

--- src/components/User.js
+++ src/components/User.js
@@ -6,7 +6,19 @@ const User =  async () => {
 
     console.log(user)
 
-    const template = `User`
+    const template = `
+       <div class="card">
+        <img src="${user.picture.large}" />
+        <div class="card-body">
+        <h1>${user.name.first} ${user.name.last}</h1>
+        <ul>
+          <li>${user.email}</li>
+          <li>${user.phone}</li>
+          <li>${user.location.city}</li>
+        </ul>
+        </div>
+       </div>
+    `
     
     return template;
 }
